61464f464ef0435f1238fa85420f10e438bcdcb5
blinkzdlfx <blinkzdlfx@gmail.com>
2026-09-11 05:11:21 +0100

    Fix Supabase anon key in storefront client

--- storefront/js/lib/supabaseClient.js
+++ storefront/js/lib/supabaseClient.js
@@ -14,6 +14,6 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const SUPABASE_URL = "https://wcyztayuulzxchkljdoo.supabase.co";
 const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndjeXp0YXl1dWx6ZWNobGpub2";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzIiwicmVmIjoid2N5enRheXV1bHoxeGNoa2xqZG9vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkwNjY3NjIsImV4cCI6MjEwNDY0Mjc2MX0.ALaX08krMhTTAR01YRBYdI_KME3CdkeSYtbHcC2e5NY";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
